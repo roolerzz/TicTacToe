86342d536d358ede5a142ef1853cc93f8992686b
Hemant Sethi <sethi.hemant@gmail.com>
2018-06-08 22:51:54 -0400

    Committing project skeleton for Console Based TicTac application.

---
+++ src/main/java/com/hemant/tictactoe/App.java
@@ -0,0 +1,13 @@
+package com.hemant.tictactoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
